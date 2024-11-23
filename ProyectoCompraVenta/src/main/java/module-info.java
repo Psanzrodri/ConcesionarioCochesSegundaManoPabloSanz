734b955326ff11221fd5edb74da0ae0aaeb417a0
Psanzrodri <149407669+Psanzrodri@users.noreply.github.com>
2024-11-23 18:09:39 +0100

    Delete ProyectoCompraVenta directory

--- ProyectoCompraVenta/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.proyectocompraventa {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.proyectocompraventa to javafx.fxml;
-    exports com.example.proyectocompraventa;
-}
